661cf58cac964e9c339c962b3d02439c6e3434c2
Jim Hinds <jahbini@jahbini.org>
2015-01-04 20:56:52 +0000

    save devo branch

--- app.js
+++ app.js
@@ -269,9 +269,43 @@ function stopRecording(){
 	}
 }
 
+function show3d () {
+	var context, dragger, height, scene, shape, width;
+
+width = 500;
+
+height = 500;
+
+shape = seen.Shapes.arrow(1,2,1,1.4).scale(height * 0.1);
+
+seen.Colors.randomSurfaces2(shape);
+
+scene = new seen.Scene({
+  model: seen.Models["default"]().add(shape),
+  viewport: seen.Viewports.center(width, height)
+});
+
+context = seen.Context('seen-canvas', scene).render();
+
+context.animate().onBefore(function(t, dt) {
+  return shape.rotx(dt * 1e-4).roty(0.7 * dt * 1e-4);
+}).start();
+
+dragger = new seen.Drag('seen-canvas', {
+  inertia: true
+});
+
+dragger.on('drag.rotate', function(e) {
+  var xform, _ref;
+  xform = (_ref = seen.Quaternion).xyToTransform.apply(_ref, e.offsetRelative);
+  shape.transform(xform);
+  return context.render();
+});
+}
+
 $(function(){
 	initAll();
 	$("#reset").prop('disabled',false).fadeTo(0,1).click(enterReset);
 	$(document).on('deviceready', initialiseSensorTag );
-
+ show3d();
 });	
