67dcdd0ceaecf82323e9797091fe2b168b366d42
Jim Hinds <jahbini@jahbini.org>
2015-01-25 07:51:08 +0000

    evening check-in

--- app.js
+++ app.js
@@ -8,58 +8,13 @@
    readings,
    calibrating=false,
    calibrate=false;
-   
-  function clearUserInterface(){
-    // Clear current values.
-    var blank = '[Waiting for value]';
-    $('#StatusData').html('Ready to connect');
-    $('#FirmwareData').html('?');
-    $('#KeypressData').html('');
-    $('#AccelerometerData').html(blank);
-    $('#MagnetometerData').html(blank);
-    $('#GyroscopeData').html(blank);
-    $('#TotalReadings').html( 0);
-    // Reset screen color.
-    setBackgroundColor('white');
-    $(":button").prop("disabled",true);
-    $("#stop").click(stopRecording);
-    $("#record").click(enterRecording).fadeTo(0,1).text('record');
-    
-    $("#reset").prop("disabled",false);
-  }
-
-  function countReadings(){
-    $('#TotalReadings').html(readings.length);
-  }
-  
-  function initDataStructures(){
-    var rtemp; 
-    reading = Backbone.Model.extend(
-      {defaults:{sensor:'gyro',x:0,y:0,z:0},
-       initialize: function(){var d= new Date(); 
-                  this.set('time',d.getTime())}
-      });
-     
-    rtemp = Backbone.Collection.extend(
-      {
-        model:'reading',
-        initialize: function() {
-          this.on('add',countReadings);
-          this.on('remove',countReadings);
-          this.on('reset',countReadings);
-        }
-      });
-
-    readings = new rtemp();
-    $('#TotalReadings').html( '0');
-  }
-  
-  function initAll(){
-    var rtemp; 
-    clearUserInterface();
-    initDataStructures();
-  }
-    
+   var console = new Console('console-log');
+// ## Hardware
+// external communications to Hardware
+//
+// set up the sensorTag and configure to recieve
+// accelerometer, magnetometer and gyro data
+//
   function initialiseSensorTag(){
     // Here sensors are set up.
     //
@@ -84,84 +39,79 @@
       .connectToClosestDevice();
   }
   
-  function enterReset(){
-    // legal to enter Reset from any state 
-    reading = false;
-    readings = null;
-    // sensortag.disconnectDevice();
-    //sensortag = evothings.tisensortag.createInstance();
-    recording=false;
+ // ## section: View
+ // routines to control or coordinate with user
+ //
+  
+  function templater(x,y,z,sensor,unit){
+      return  sensor+ ' x=' + (x >= 0 ? '+' : '') + x.toFixed(3) + unit +' -- '
+      + 'y=' + (y >= 0 ? '+' : '') + y.toFixed(3) + unit + ' -- '
+      + 'z=' + (z >= 0 ? '+' : '') + z.toFixed(3) + unit;
+  }
+  
+  function pointFormat(p,unit,precision){
+      if(!precision) precision = 3;
+      if(!unit) unit = 'v';
+      return  ' x=' + (p.x >= 0 ? '+' : '') + p.x.toFixed(precision) + unit +' -- '
+      + 'y=' + (p.y >= 0 ? '+' : '') + p.y.toFixed(precision) + unit + ' -- '
+      + 'z=' + (p.z >= 0 ? '+' : '') + p.z.toFixed(precision) + unit;
+  }
+ 
+  function clearUserInterface(){
+    // Clear current values.
+    var blank = '[Waiting for value]';
+    $('#StatusData').html('Ready to connect');
+    $('#FirmwareData').html('?');
+    $('#KeypressData').html('');
+    $('#AccelerometerData').html(blank);
+    $('#MagnetometerData').html(blank);
+    $('#GyroscopeData').html(blank);
+    $('#TotalReadings').html( 0);
+    // Reset screen color.
+    setBackgroundColor('white');
+    $(":button").prop("disabled",true);
+    $("#stop").click(stopRecording);
+    $("#record").click(enterRecording).fadeTo(0,1).text('record');
+    $("#reset").prop("disabled",false);
+  }
+
+  function countReadings(){
+    $('#TotalReadings').html(readings.length);
+  }
+ 
+ // ## Section: Data Structures
+ // Routines to create and handle data structures and interfaces to them
+ //
+  function initDataStructures(){
+    var rtemp; 
+    reading = Backbone.Model.extend(
+      {defaults:{sensor:'gyro',x:0,y:0,z:0},
+       initialize: function(){var d= new Date(); 
+                  this.set('time',d.getTime())}
+      });
+     
+    rtemp = Backbone.Collection.extend(
+      {
+        model:'reading',
+        initialize: function() {
+          this.on('add',countReadings);
+          this.on('remove',countReadings);
+          this.on('reset',countReadings);
+        }
+      });
+
+    readings = new rtemp();
+  }
+  // ## Section State Handlers
+  
+  
+  function initAll(){
+    var rtemp; 
+    clearUserInterface();
     initDataStructures();
-    enterConnected();
+    $('#TotalReadings').html( '0');
   }
-  
-  function enterConnected(){
-    // enable the recording button
-    connected = true;
-    $("#record").prop('disabled',false).fadeTo(100,1).text('record').click(enterRecording);
-    $("#stop").prop('disabled',true);
-    $("#upload").prop('disabled',true);
-    $("#calibrate").prop('disabled',false);
-  }
-  
-  function enterCalibrating(){
-    $("#record").prop('disabled',true);
-    $("#stop").prop('disabled',true);
-    $("#upload").prop('disabled',true);
-    calibrating=true;
-  }
-  
-  function enterRecording(){
-    $("#record").prop('disabled',true).text('recording').fadeTo(200,0.6);
-    $("#stop").prop('disabled',false).fadeTo(100,1).click(enterReview);
-    $("#upload").prop('disabled',true);
-    recording=true;
-  }
-  
-  function enterReview(){
-    $("#stop").prop('disabled',true).fadeTo(100,0.5);
-    $("#record").prop('disabled',true).text('recorded').fadeTo(200,0.3);
-    $("#upload").prop('disabled',false).click(enterUpload).fadeTo(100,1)
-   recording=false;	
-  }
-  
-  function enterUpload(){
-    var hopper,brainDump;
-    /* eliminate empty uploads per : https://github.com/jahbini/stagapp/issues/15 */
-    if(!readings.length) return;
-    hopper = Backbone.Model.extend({url:"/trajectory"});
-    brainDump = new hopper({readings: readings});
-    brainDump.save();
-    readings.reset();
-    enterConnected();
-  }
-  
-  function statusHandler(status){
-    if ('Sensors online' == status){
-      enterConnected();
-    }
-    
-    if ('Device data available' == status){
-      $('#FirmwareData').html(sensortag.getFirmwareString());
-    }
-    $('#StatusData').html(status);
-  }
-  
-  function errorHandler(error){
-    console.log('Error: ' + error)
-    if ('disconnected' == error){
-      connected = false;
-      clearUserInterface();
-      // If disconneted attempt to connect again.
-      setTimeout(
-        function() { sensortag.connectToClosestDevice() },
-        1000)
-    }
-  }
-
-  // calculations implemented as based on TI wiki pages
-  // http://processors.wiki.ti.com/index.php/SensorTag_User_Guide
-
+ // ### subsection State handlers that depend on the View 
   function keypressHandler(data){
     var left=0,right=0,string;
     
@@ -190,29 +140,104 @@
     $('KeypressData').html(string);
   }
   
-  function templater(x,y,z,sensor,unit){
-      return  sensor+ ' x=' + (x >= 0 ? '+' : '') + x.toFixed(3) + unit +' -- '
-      + 'y=' + (y >= 0 ? '+' : '') + y.toFixed(3) + unit + ' -- '
-      + 'z=' + (z >= 0 ? '+' : '') + z.toFixed(3) + unit;
+
+  function enterReset(){
+    // legal to enter Reset from any state 
+    reading = false;
+    readings = null;
+    // sensortag.disconnectDevice();
+    //sensortag = evothings.tisensortag.createInstance();
+    recording=false;
+    initDataStructures();
+    enterConnected();
+    $('#TotalReadings').html( '0');
   }
   
-  function pointFormat(p,unit,precision){
-      if(!precision) precision = 3;
-      if(!unit) unit = 'v';
-      return  ' x=' + (p.x >= 0 ? '+' : '') + p.x.toFixed(precision) + unit +' -- '
-      + 'y=' + (p.y >= 0 ? '+' : '') + p.y.toFixed(precision) + unit + ' -- '
-      + 'z=' + (p.z >= 0 ? '+' : '') + p.z.toFixed(precision) + unit;
+  function enterConnected(){
+    // enable the recording button
+    connected = true;
+    $("#record").prop('disabled',false).fadeTo(100,1).text('record').click(enterRecording);
+    $("#stop").prop('disabled',true);
+    $("#upload").prop('disabled',true);
+    $("#calibrate").prop('disabled',false).click(enterCalibrating);
   }
- 
+  
+  function enterCalibrating(){
+    $("#record").prop('disabled',true);
+    $("#stop").prop('disabled',true);
+    $("#upload").prop('disabled',true);
+    $("#calibrate").text("button 1 active").click(exitCalibrating);
+    calibrating=true;
+  }
+  
+  function exitCalibrating(){
+    calibrating=false;
+    $("#calibrate").text("calibrate").click(enterCalibrating);
+  }
+  
+  function enterRecording(){
+    $("#record").prop('disabled',true).text('recording').fadeTo(200,0.6);
+    $("#stop").prop('disabled',false).fadeTo(100,1).click(enterReview);
+    $("#upload").prop('disabled',true);
+    recording=true;
+  }
+  
+  function enterReview(){
+    $("#stop").prop('disabled',true).fadeTo(100,0.5);
+    $("#record").prop('disabled',true).text('recorded').fadeTo(200,0.3);
+    $("#upload").prop('disabled',false).click(enterUpload).fadeTo(100,1)
+   recording=false;	
+  }
+  
+  function enterUpload(){
+    var hopper,brainDump;
+//    eliminate empty uploads per : https://github.com/jahbini/stagapp/issues/15 */
+    if(!readings.length) return;
+    hopper = Backbone.Model.extend({url:"/trajectory"});
+    brainDump = new hopper({readings: readings});
+    brainDump.save();
+    readings.reset();
+    enterConnected();
+  }
+ //
+ // ### Subsection State Handlers that depend on the Hardware
+  function statusHandler(status){
+    if ('Sensors online' == status){
+      enterConnected();
+    }
+    
+    if ('Device data available' == status){
+      $('#FirmwareData').html(sensortag.getFirmwareString());
+    }
+    $('#StatusData').html(status);
+  }
+  
+  function errorHandler(error){
+    console.log('Error: ' + error)
+    if ('disconnected' == error){
+      connected = false;
+      clearUserInterface();
+      // If disconneted attempt to connect again.
+      setTimeout(
+        function() { sensortag.connectToClosestDevice() },
+        1000)
+    }
+  }
+
+// ## subsection State routines that depend on hardware and update the view or data structures
+// calculations implemented as based on TI wiki pages
+// http://processors.wiki.ti.com/index.php/SensorTag_User_Guide
  var accelerometerHandler = readingHandler(
    {sensor:'accel',
     source:sensortag.getAccelerometerValues,
     units:'G',
+    calibrator:calibratorMid,
     viewer:viewSensor('accel-view',0.8),
     htmlID:'AccelerometerData'}
     );
  var magnetometerHandler = readingHandler(
    { sensor:'mag',
+    calibrator:calibratorMid,
      source:sensortag.getMagnetometerValues,
      units:'&micro;T',
      viewer:viewSensor('magnet-view',1),
@@ -220,32 +245,106 @@
      );
  var gyroscopeHandler = readingHandler(
    {sensor:'gyro',
+    calibrator:calibratorMid,
     source:sensortag.getGyroscopeValues,
     viewer:viewSensor('gyro-view',1),
     htmlID:'GyroscopeData'
    }
    );
-     
+ 
+ function calibratorAverage(dataCondition,calibrate,calibrating){
+   try{ 
+     var tH;
+     if(dataCondition.dataHistory === undefined){
+       dataCondition.dataHistory = {
+         grandTotal:seen.P(0,0,0),
+         grandAverage:seen.P(0,0,0),
+         totalReadings:1
+       };
+     }
+     tH = dataCondition.dataHistory;
+   // If button one is down, and we are in a calibration mode, gather all values
+     if(calibrating){
+       tH.grandTotal.add(dataCondition.curValue);
+       tH.totalReadings++;
+       tH.grandAverage=tH.grandTotal.copy().divide(tH.totalReadings);
+     }
+     dataCondition.cookedValue = dataCondition.curValue.copy().subtract(tH.grandAverage);
+   } catch (e) {
+     console.log(e.message);
+   }
+ }
+ 
+  function split(raw,lo,hi){
+    return raw-(hi-lo)/2.0;
+  } 
+ function calibratorMid(dataCondition,calibrate,calibrating){
+   try{ 
+     var tH;
+     if(dataCondition.dataHistory === undefined){
+       dataCondition.dataHistory = {
+         max:dataCondition.cookedValue.copy(),
+         min:dataCondition.cookedValue.copy()
+       };
+     }
+     tH = dataCondition.dataHistory;
+     if(dataCondition.cookedValue.x>tH.max.x)tH.max.x = dataCondition.cookedValue.x;
+     if(dataCondition.cookedValue.y>tH.max.y)tH.max.y = dataCondition.cookedValue.y;
+     if(dataCondition.cookedValue.z>tH.max.z)tH.max.z = dataCondition.cookedValue.z;
+     if(dataCondition.cookedValue.x<tH.min.x)tH.min.x = dataCondition.cookedValue.x;
+     if(dataCondition.cookedValue.y<tH.min.y)tH.min.y = dataCondition.cookedValue.y;
+     if(dataCondition.cookedValue.z<tH.min.z)tH.min.z = dataCondition.cookedValue.z;
+     dataCondition.cookedValue.x = split(dataCondition.cookedValue.x,tH.min.x,tH.max.x);
+     dataCondition.cookedValue.y = split(dataCondition.cookedValue.y,tH.min.y,tH.max.y);
+     dataCondition.cookedValue.z = split(dataCondition.cookedValue.z,tH.min.z,tH.max.z);
+   } catch (e) {
+     console.log(e.message);
+   }
+ }
+ 
+ function calibratorSmooth(dataCondition,calibrate,calibrating){
+   try{ 
+     if(dataCondition.dataHistory === undefined){
+       dataCondition.dataHistory = {
+         runningSum: dataCondition.cookedValue.copy()
+       };
+     }
+     dataCondition.cookedValue = dataCondition.dataHistory.runningSum.multiply(0.75).
+      add(dataCondition.cookedValue.copy().multiply(0.25)).copy();
+   } catch (e) {
+     console.log(e.message);
+   }
+ }
+ 
+ //
  //#  ## readingHandler()
  //# create and return a function to handle a sensor's new data
   function readingHandler(o){
+    var dataCondition = {
+      curValue:seen.P(0,0,0),
+      cookedValue:seen.P(0,0,0),
+      dataHistory:undefined
+    };
     // if there is no calibration function, just use a null offset
-    if(!o.calibrator) o.calibrator = function(){return seen.P(0,0,0)};
+    if(!o.calibrator) o.calibrator = function(d){d.cookedValue = d.curValue;};
     if(!o.units) o.units = '';
     
     return function (data){
       // data points from Evothings library are seen.Point NOT compatible as sources
-      var r=o.source(data); 
-      var p = new seen.P(r.x,r.y,r.z); 
-      p.subtract(o.calibrator());
+      var r=o.source(data),p; 
+      // get the sensor data and pass to conditioner
+      dataCondition.curValue = new seen.P(r.x,r.y,r.z); 
+      dataCondition.cookedValue = new seen.P(r.x,r.y,r.z); 
+      o.calibrator(dataCondition,calibrate,calibrating);
+      p=dataCondition.cookedValue;
       if(recording) readings.push(
         new reading({sensor:o.sensor,x:p.x,y:p.y,z:p.z,raw:_.toArray(data)})
+        
         );
-      $(o.htmlID).html(templater(p.x,p.y,p.z,o.sensor,o.units) );
+      $('#'+o.htmlID).html(templater(p.x,p.y,p.z,o.sensor,o.units) );
       o.viewer(p.x,p.y,p.z);
     };
   }
-
   function setBackgroundColor(color)
   {
     document.documentElement.style.background = color
@@ -383,6 +482,7 @@ viewGyro(-5,0,0);
 */
 
 $(function(){
+  console.log('hello');
   initAll();
   $("#reset").prop('disabled',false).fadeTo(0,1).click(enterReset);
   $(document).on('deviceready', initialiseSensorTag );
