f7592c59555f1b058c4ee44202beac207ef364c2
Jim Hinds <RetroJim@Retro.local>
2015-09-04 06:13:40 +1000

    revert from development mode

--- public/app.js
+++ public/app.js
@@ -17924,7 +17924,7 @@ if ((typeof module !== "undefined" && module !== null ? module.exports : void 0)
 
 Pylon.set('spearCount', 5);
 
-development = true;
+development = false;
 
 if (development) {
   Pylon.set('hostUrl', "http://Retro.local:3000/");
