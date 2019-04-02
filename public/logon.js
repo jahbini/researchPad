43dd423b2fcf1dcbbdd0f1fb825670ec2c544046
James jahbini Hinds <jim@jahbini.org>
2019-04-03 07:23:00 +1000

    2.9.3

--- public/logon.js
+++ public/logon.js
@@ -36,3 +36,4 @@ console.log('my new logon.js loaded - live system with events');
 /*Retrotope App Version  "2.9.0" */
 /*Retrotope App Version  "2.9.1" */
 /*Retrotope App Version  "2.9.2" */
+/*Retrotope App Version  "2.9.3" */
