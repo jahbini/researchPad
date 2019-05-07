c1c9be2d1959bc89ea210911f064e7082573779b
James jahbini Hinds <jim@jahbini.org>
2019-05-07 14:26:47 +1000

    3.0.1-test

--- public/app.js
+++ public/app.js
@@ -2884,7 +2884,7 @@ exports.state = new State;
 
 
 },{"../lib/buglog.coffee":3,"backbone":30,"underscore":40}],21:[function(require,module,exports){
-module.exports = '2.9.8-test';
+module.exports = '3.0.1-test';
 
 
 
